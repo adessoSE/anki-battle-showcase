4dcd9137c4eff6f82fb11b4b371e1f7a825d99ad
chai <kenghong.chai@tu-dortmund.de>
2018-01-15 11:25:10 +0100

    implemented some commands

--- src/main/java/com/commands/AccelerateCommand.java
+++ src/main/java/com/commands/AccelerateCommand.java
@@ -1,10 +1,18 @@
 package com.commands;
 
+import de.adesso.anki.battle.world.bodies.Vehicle;
+
 public class AccelerateCommand extends Command {
 	private int desiredSpeed;
 	
 	public AccelerateCommand() {
 		this.desiredSpeed = desiredSpeed;
+	}
+	
+	
+	@Override
+	public void execute(Vehicle vehicle){
+		vehicle.setSpeed(this.desiredSpeed);
 	}
 	
 }
--- src/main/java/com/commands/ShieldCommand.java
+++ src/main/java/com/commands/ShieldCommand.java
@@ -1,5 +1,10 @@
 package com.commands;
 
 public class ShieldCommand extends Command {
+	public ShieldCommand () {
+	}
 	
+	public void execute() {
+		//remove Shield from Inventory
+	}
 }
