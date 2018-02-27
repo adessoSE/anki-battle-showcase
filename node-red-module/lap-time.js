153beb41e50c5ad8ce4e1e1cf4fce4fe8ae0e8d5
Chai <kenghong.chai@tu-dortmund.de>
2018-02-27 14:46:07 +0100

    added node that measures lap time

---
+++ node-red-module/lap-time.js
@@ -0,0 +1,30 @@
+module.exports = function(RED) {
+    function lapTime(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+			var context = this.context();
+			if (msg.payload.currentRoadpiece == "startPiece"){
+				var d = new Date();
+				var time = d.getTime();
+				var startTime = context.get('startTime')|| time
+				context.set('startTime',time);
+			}
+		var d = new Date();
+		var time = d.getTime();
+		var lapTime = time - context.get("startTime")
+
+
+		if (msg.payload.currentRoadpiece == "endPiece"){
+			var d = new Date();
+			var endTime = d.getTime();
+			var startTime = context.get("startTime");
+			var lapTime = endTime - startTime;
+		}
+		newMsg = {"time" : lapTime/1000};
+		node.log(newMsg);
+		node.send( newMsg);
+        });
+    }
+    RED.nodes.registerType("Rundenzeit",lapTime);
+}
