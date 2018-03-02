003d27871ead064925faa93043f1bb0b5e7e718f
Yannick Eckey <yannick.eckey@googlemail.com>
2018-03-02 15:10:15 +0100

    Never transfer reversed roadpieces to frontend

--- src/main/java/de/adesso/anki/battle/transfer/RoadmapDTO.java
+++ src/main/java/de/adesso/anki/battle/transfer/RoadmapDTO.java
@@ -1,6 +1,7 @@
 package de.adesso.anki.battle.transfer;
 
 import de.adesso.anki.battle.world.bodies.Roadmap;
+import de.adesso.anki.battle.world.bodies.roadpieces.ReverseRoadpiece;
 import de.adesso.anki.battle.world.bodies.roadpieces.Roadpiece;
 import lombok.val;
 
@@ -18,13 +19,21 @@ public class RoadmapDTO {
     public List<Roadpiece> getRoadpieces() {
         val list = new LinkedList<Roadpiece>();
         val anchor = roadmap.getAnchor();
-        list.add(anchor);
+        addWithChecks(list, anchor);
 
         for (Roadpiece i = anchor.getNext(); i.getNext() != null && i != anchor; i = i.getNext()) {
-            list.add(i);
+            addWithChecks(list, i);
         }
 
         return list;
+    }
+
+    private void addWithChecks(LinkedList<Roadpiece> list, Roadpiece i) {
+        if (i instanceof ReverseRoadpiece) {
+            list.add(i.reverse());
+        } else {
+            list.add(i);
+        }
     }
 
 }
