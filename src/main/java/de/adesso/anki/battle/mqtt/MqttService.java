29049406e1143c975dd97b3679f31d27841a9493
Yannick Eckey <yannick.eckey@googlemail.com>
2018-03-05 13:41:59 +0100

    Catch exceptions while parsing MQTT messages

--- src/main/java/de/adesso/anki/battle/mqtt/MqttService.java
+++ src/main/java/de/adesso/anki/battle/mqtt/MqttService.java
@@ -42,59 +42,61 @@ public class MqttService {
                 }
 
                 @Override
-                public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-                    log.info("MQTT message arrived: topic="+s+"; message="+mqttMessage.toString());
-                    String temp = mqttMessage.toString();
-                    byte [] hm = mqttMessage.getPayload() ;
-                    JSONObject json = new JSONObject(mqttMessage.toString());
-                    
-                    //get topic, where topic is identifier for vehicle 
-                    //get corresponding vehicle v
-                    // v.exexute command 
-                    String topic = s.split("_")[0]; 
-                    List<DynamicBody>vehicles = world.getDynamicBodies();
-                   	Vehicle wantedVehicle = null; 
-                    for (int i = 0 ; i < vehicles.size(); i++){
-                    	Vehicle currentVehicle = ((Vehicle)vehicles.get(i));
-                    	if (topic.equals (currentVehicle.getName())) {
-                    		wantedVehicle = currentVehicle;
-                    		break;
-                    	}
-                    }
-                    String commandType = (String) json.get("type");
-                    int speed; 
-                    Command command = null ; 
-                    int track;
-                    switch(commandType) {
-                    	case("accelerate"): 
-                    		speed = Integer.parseInt((String)  json.get("velocity"));
-                    		command = new AccelerateCommand(speed) ;
-                            break;
-                        case("brake"):
-                            speed = Integer.parseInt((String)  json.get("velocity"));
-                            command = new BrakeCommand(speed) ;
-                            break;
-                        case("change track"):
-                            val lane = Double.parseDouble((String)  json.get("track"));
-                            command = new ChangeLaneCommand(lane);
-                            break;
-                    	case("uTurn"): 
-                    		command = new UTurnCommand() ;
-                    		break;
-                    	case("fireRocket"): 
-                    		command = new FireRocketCommand("") ;
-                    		break;
-                    	case("putMine"): 
-                    		command = new PutMineCommand() ;
-                    		break;
-                    } 
-                    if (command != null) {
-                        command.execute(wantedVehicle);	
-                    }
-                    else {
-                    	log.debug("No Command");
-                    }
+                public void messageArrived(String s, MqttMessage mqttMessage) {
+                    try {
+                        log.info("MQTT message arrived: topic=" + s + "; message=" + mqttMessage.toString());
+                        String temp = mqttMessage.toString();
+                        byte[] hm = mqttMessage.getPayload();
+                        JSONObject json = new JSONObject(mqttMessage.toString());
 
+                        //get topic, where topic is identifier for vehicle
+                        //get corresponding vehicle v
+                        // v.exexute command
+                        String topic = s.split("_")[0];
+                        List<DynamicBody> vehicles = world.getDynamicBodies();
+                        Vehicle wantedVehicle = null;
+                        for (int i = 0; i < vehicles.size(); i++) {
+                            Vehicle currentVehicle = ((Vehicle) vehicles.get(i));
+                            if (topic.equals(currentVehicle.getName())) {
+                                wantedVehicle = currentVehicle;
+                                break;
+                            }
+                        }
+                        String commandType = (String) json.get("type");
+                        int speed;
+                        Command command = null;
+                        int track;
+                        switch (commandType) {
+                            case ("accelerate"):
+                                speed = Integer.parseInt((String) json.get("velocity"));
+                                command = new AccelerateCommand(speed);
+                                break;
+                            case ("brake"):
+                                speed = Integer.parseInt((String) json.get("velocity"));
+                                command = new BrakeCommand(speed);
+                                break;
+                            case ("change track"):
+                                val lane = Double.parseDouble((String) json.get("track"));
+                                command = new ChangeLaneCommand(lane);
+                                break;
+                            case ("uTurn"):
+                                command = new UTurnCommand();
+                                break;
+                            case ("fireRocket"):
+                                command = new FireRocketCommand("");
+                                break;
+                            case ("putMine"):
+                                command = new PutMineCommand();
+                                break;
+                        }
+                        if (command != null) {
+                            command.execute(wantedVehicle);
+                        } else {
+                            log.debug("No Command");
+                        }
+                    } catch (Exception e) {
+                        log.error("Error while parsing MQTT message", e);
+                    }
                 }
 
                 @Override
