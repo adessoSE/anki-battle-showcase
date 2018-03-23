153f688a915fec790913e61b62a2154be4f16208
Yannick Eckey <yannick.eckey@googlemail.com>
2018-03-23 15:59:36 +0100

    Fix: Vehicles would not subscribe to changed behavior

--- src/main/java/de/adesso/anki/battle/input/WebsocketInput.java
+++ src/main/java/de/adesso/anki/battle/input/WebsocketInput.java
@@ -1,9 +1,11 @@
 package de.adesso.anki.battle.input;
 
+import de.adesso.anki.battle.mqtt.MqttService;
 import de.adesso.anki.battle.transfer.CommandDTO;
 import de.adesso.anki.battle.world.World;
 import de.adesso.anki.battle.world.bodies.Vehicle;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,9 @@ public class WebsocketInput {
 
     @Autowired
     private World world;
+
+    @Autowired
+    private MqttService mqtt;
 
     @MessageMapping("/commands")
     public void handleCommand(CommandDTO dto) {
@@ -30,6 +35,12 @@ public class WebsocketInput {
             if (body instanceof Vehicle) {
                 log.debug("Changed username: " + username);
                 ((Vehicle) body).setName(username);
+
+                try {
+                    mqtt.subscribe(username + "_sub");
+                } catch (MqttException e) {
+                    log.error("Could not subscribe new username", e);
+                }
             }
         });
     }
