ac799e00a65194d4bce621cbbb85f2cdde4db4dd
Yannick Eckey <yannick.eckey@adesso.de>
2018-03-28 15:34:59 +0200

    Refactor Command
    - Make Command class abstract
    - Make default constructor protected
    - Adjust code style

--- src/main/java/com/commands/Command.java
+++ src/main/java/com/commands/Command.java
@@ -5,19 +5,17 @@ import de.adesso.anki.battle.world.bodies.Vehicle;
 import java.time.LocalTime;
 
 
-public class Command {
-	private LocalTime time ;
+public abstract class Command {
+
+	private LocalTime time;
 	
-	public Command () {
+	protected Command() {
 		time = LocalTime.now();
 	}
-	public LocalTime getTime () {
+
+	public LocalTime getTime() {
 		return time;
-	} 
-	
-	
-	public void execute(Vehicle vehicle) {
-		
 	}
 	
+	public abstract void execute(Vehicle vehicle);
 }
