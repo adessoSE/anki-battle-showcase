f72876b1610f29065fab9f26730355b4ae667d0d
Yannick Eckey <yannick.eckey@adesso.de>
2018-04-23 17:57:13 +0200

    Add start script for easy setup

---
+++ start_showcase.ps1
@@ -0,0 +1,10 @@
+﻿.\mvnw.cmd initialize install -DskipTests
+cd .node-red
+..\node\node.exe ..\node\node_modules\npm\bin\npm-cli.js install --no-optional
+$node_red = Start-Process -FilePath ..\node\node.exe -ArgumentList .\node_modules\node-red\red.js,"-u","." -PassThru -NoNewWindow
+cd ..
+try {
+    java -jar .\target\anki-battle-0.3.0-SNAPSHOT.jar
+} finally {
+    Stop-Process $node_red
+}
