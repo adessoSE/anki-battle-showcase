fdb75e3cf60d4888d9d24be77edd872177bf379a
Yannick Eckey <yannick.eckey@adesso.de>
2018-04-23 18:31:06 +0200

    Fix start script

--- start_showcase.ps1
+++ start_showcase.ps1
@@ -1,9 +1,13 @@
-﻿.\mvnw.cmd initialize install -DskipTests
+﻿$Host.UI.RawUI.BackgroundColor = 'Black'
+[console]::BackgroundColor = "black"
+.\mvnw.cmd initialize
+.\mvnw.cmd install -DskipTests
 cd .node-red
-..\node\node.exe ..\node\node_modules\npm\bin\npm-cli.js install --no-optional
+..\node\node.exe ..\node\node_modules\npm\bin\npm-cli.js install
 $node_red = Start-Process -FilePath ..\node\node.exe -ArgumentList .\node_modules\node-red\red.js,"-u","." -PassThru -NoNewWindow
 cd ..
 try {
+    Start-Sleep 5
     java -jar .\target\anki-battle-0.3.0-SNAPSHOT.jar
 } finally {
     Stop-Process $node_red
