24cd5fc206f9ee2b4cc5505f6d66fe92bdc27d8d
Yannick Eckey <yannick.eckey@googlemail.com>
2018-04-24 09:51:47 +0200

    Add start script for ease of use (#26)
    Closes #4.

---
+++ start_showcase.ps1
@@ -0,0 +1,14 @@
+﻿$Host.UI.RawUI.BackgroundColor = 'Black'
+[console]::BackgroundColor = "black"
+.\mvnw.cmd initialize
+.\mvnw.cmd install -DskipTests
+cd .node-red
+..\node\node.exe ..\node\node_modules\npm\bin\npm-cli.js install
+$node_red = Start-Process -FilePath ..\node\node.exe -ArgumentList .\node_modules\node-red\red.js,"-u","." -PassThru -NoNewWindow
+cd ..
+try {
+    Start-Sleep 5
+    java -jar .\target\anki-battle-0.3.0-SNAPSHOT.jar
+} finally {
+    Stop-Process $node_red
+}
